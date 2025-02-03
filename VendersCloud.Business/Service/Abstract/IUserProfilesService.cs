8a4c1ac761ce16f85d289a94351d150c609ce0dc
Anusheel Sharma <158999628+AnusheelSharma1@users.noreply.github.com>
2025-02-03 17:22:53 +0530

    changes added

--- VendersCloud.Business/Service/Abstract/IUserProfilesService.cs
+++ VendersCloud.Business/Service/Abstract/IUserProfilesService.cs
@@ -1,11 +1,12 @@
 ﻿
 using VendersCloud.Business.Entities.DataModels;
+using VendersCloud.Business.Entities.Dtos;
 
 namespace VendersCloud.Business.Service.Abstract
 {
     public interface IUserProfilesService
     {
         Task<bool> InsertUserProfileAsync(int userId, int profileId);
-        Task<List<UserProfiles>> GetProfileRole(int userId);
+        Task<List<UserProfileDto>> GetProfileRole(int userId);
     }
 }
