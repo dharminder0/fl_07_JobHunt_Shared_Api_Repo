e7b4edce8140df140fcdb335e77547bfb6740e6c
Anusheel Sharma <158999628+AnusheelSharma1@users.noreply.github.com>
2025-02-06 20:21:27 +0530

    added api's to controller

--- VendersCloud.Business/Service/Concrete/OrgLocationService.cs
+++ VendersCloud.Business/Service/Concrete/OrgLocationService.cs
@@ -1,8 +1,47 @@
-﻿using VendersCloud.Business.Service.Abstract;
+﻿using VendersCloud.Business.Entities.DataModels;
+using VendersCloud.Business.Service.Abstract;
+using VendersCloud.Data.Repositories.Abstract;
 
 namespace VendersCloud.Business.Service.Concrete
 {
-    public class OrgLocationService :IOrgLocationService
+    public class OrgLocationService : IOrgLocationService
     {
+        private readonly IOrgLocationRepository _orgLocationRepository;
+        public OrgLocationService(IOrgLocationRepository orgLocationRepository)
+        {
+            _orgLocationRepository = orgLocationRepository;
+        }
+
+        public async Task<bool> UpsertLocation(OrgLocation location)
+        {
+            try
+            {
+                if (location == null)
+                {
+                    return false;
+                }
+                var response = await _orgLocationRepository.UpsertLocation(location);
+                return response;
+            }
+            catch (Exception ex) {
+                return false;
+            }
+        }
+
+        public async Task<List<OrgLocation>> GetOrgLocation(string orgCode)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(orgCode)) {
+                    return null;
+                    }
+
+                var response= await _orgLocationRepository.GetOrgLocation(orgCode);
+                return response;
+            }
+            catch (Exception ex) { 
+            return null;
+            }
+        }
     }
 }
