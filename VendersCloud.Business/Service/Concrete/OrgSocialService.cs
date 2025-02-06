4ebf6d5a5561b09e9d549899ceb318b71713a3dc
Anusheel Sharma <158999628+AnusheelSharma1@users.noreply.github.com>
2025-02-06 20:47:46 +0530

    added social profile api to contoller

--- VendersCloud.Business/Service/Concrete/OrgSocialService.cs
+++ VendersCloud.Business/Service/Concrete/OrgSocialService.cs
@@ -1,8 +1,46 @@
-﻿using VendersCloud.Business.Service.Abstract;
+﻿using VendersCloud.Business.Entities.DataModels;
+using VendersCloud.Business.Service.Abstract;
+using VendersCloud.Data.Repositories.Abstract;
 
 namespace VendersCloud.Business.Service.Concrete
 {
     public class OrgSocialService: IOrgSocialService
     {
+        private readonly IOrgSocialRepository _orgSocialRepository;
+        public OrgSocialService(IOrgSocialRepository orgSocialRepository)
+        {
+            _orgSocialRepository = orgSocialRepository;
+        }
+
+        public async Task<bool> UpsertSocialProfile(OrgSocial social)
+        {
+            try
+            {
+                if (social == null) {
+                    return false;
+                }
+                var response= await _orgSocialRepository.UpsertSocialProfile(social);
+                return response;
+            }
+            catch(Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public async Task<List<OrgSocial>> GetOrgSocialProfile(string orgCode)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(orgCode)) {
+                    return null;
+                }
+                var response= await _orgSocialRepository.GetOrgSocialProfile(orgCode);
+                return response;
+            }
+            catch (Exception ex) {
+                return null;
+            }
+        }
     }
 }
--- VendersCloud/Controllers/OrgSocialController.cs
+++ VendersCloud/Controllers/OrgSocialController.cs
@@ -1,9 +1,54 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using VendersCloud.Business.Entities.DataModels;
+using VendersCloud.Business.Service.Abstract;
+using VendersCloud.Business.Service.Concrete;
 
 namespace VendersCloud.WebApi.Controllers
 {
     [ApiController]
     public class OrgSocialController : BaseApiController
     {
+        private readonly IOrgSocialService _orgSocialService;
+        public OrgSocialController(IOrgSocialService orgSocialService)
+        {
+            _orgSocialService= orgSocialService;
+        }
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [HttpPost]
+        [Route("api/v1/orgSocial/upsert-profile")]
+
+        public async Task<IActionResult> UpsertSocialProfile(OrgSocial social)
+        {
+            try
+            {
+                var result= await _orgSocialService.UpsertSocialProfile(social);
+                return Json(result);
+            }
+            catch (Exception ex) { 
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [HttpGet]
+        [Route("api/v1/orgSocial/get-profile")]
+        public async Task<IActionResult> GetOrgSocialProfile(string orgCode)
+        {
+            try { 
+                var result = await _orgSocialService.GetOrgSocialProfile(orgCode);
+                return Json(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
     }
 }
