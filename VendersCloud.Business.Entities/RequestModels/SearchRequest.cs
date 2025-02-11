394422458d2728c92d807191d90952863c0161bb
Anusheel Sharma <158999628+AnusheelSharma1@users.noreply.github.com>
2025-02-11 12:42:27 +0530

    changes done

--- VendersCloud.Business.Entities/RequestModels/SearchRequest.cs
+++ VendersCloud.Business.Entities/RequestModels/SearchRequest.cs
@@ -2,11 +2,11 @@
 {
     public class SearchRequest
     {
-        public string SearchText { get; set; } = "";
-        public string Technology { get; set; } = "";
-        public int? Resource { get; set; } = null;
+        public string SearchText { get; set; } 
+        public List<string> Technology { get; set; } 
+        public List<string> Resource { get; set; }
         public List<string> Role { get; set; }
-        public int? Strength { get; set; } = null;
+        public List<string> Strength { get; set; } 
         public int Page { get; set; } = 1;
         public int PageSize { get; set; } = 10;
     }
