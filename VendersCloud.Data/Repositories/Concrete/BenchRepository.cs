95cc5c11f0c660ca17b5140423591d36cfd7a1de
Anusheel Sharma <158999628+AnusheelSharma1@users.noreply.github.com>
2025-02-27 16:32:48 +0530

    added changes

---
+++ VendersCloud.Data/Repositories/Concrete/BenchRepository.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+using VendersCloud.Business.Entities.DataModels;
+using VendersCloud.Data.Repositories.Abstract;
+
+namespace VendersCloud.Data.Repositories.Concrete
+{
+    public class BenchRepository : StaticBaseRepository<Resources>, IBenchRepository
+    {
+        public BenchRepository(IConfiguration configuration) : base(configuration)
+        {
+
+        }
+    }
+}
