76118f591b90dd49bb78ff3d6ff1a18da5228389
Sumit6995 <Sumitvats369@gmail.com>
2025-05-20 13:07:21 +0530

    Update ResourcesRepository.cs

--- VendersCloud.Data/Repositories/Concrete/ResourcesRepository.cs
+++ VendersCloud.Data/Repositories/Concrete/ResourcesRepository.cs
@@ -101,7 +101,7 @@ namespace VendersCloud.Data.Repositories.Concrete
             var applicationsData = dbInstance.Select<Applications>(sql, new { requirementId }).ToList();
                 foreach (var app in applicationsData)
                 {
-                    var statusList = await benchRepository.GetStatusHistoryByApplicantId(app.ResourceId);
+                    var statusList = await benchRepository.GetStatusHistoryByApplicantId(app.Id);
                     if (statusList != null && statusList.Any())
                     {
                         app.Status = statusList.Select(v => v.Status).OrderByDescending(v => v).First();
