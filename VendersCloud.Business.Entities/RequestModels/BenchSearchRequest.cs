a0afeb4297ccddc68e1e43e710c7173945d15de1
Sumit6995 <Sumitvats369@gmail.com>
2025-05-30 14:43:52 +0530

    Add changes in api

--- VendersCloud.Business.Entities/RequestModels/BenchSearchRequest.cs
+++ VendersCloud.Business.Entities/RequestModels/BenchSearchRequest.cs
@@ -7,5 +7,6 @@
         public List<int> Availability { get; set; }
         public int Page { get; set; } = 1;
         public int PageSize { get; set; } = 10;
+        public int ? TopSkillId { get; set; }
     }
 }
