1298c3c179a0f17c6142291b057c645592c77908
Sumit6995 <Sumitvats369@gmail.com>
2025-07-28 12:32:23 +0530

    Update OrgProfilesService.cs

--- VendersCloud.Business/Service/Concrete/OrgProfilesService.cs
+++ VendersCloud.Business/Service/Concrete/OrgProfilesService.cs
@@ -44,10 +44,18 @@ namespace VendersCloud.Business.Service.Concrete
                 List<PartnerVendorRel> orgRelationshipData = new List<PartnerVendorRel>();
                 var data = await _orgProfilesRepository.SearchOrganizationsDetails(request);
                 if (data?.List == null || !data.List.Any())
-                    return null;
+                {
+                    return new PaginationDto<OrganizationDto>
+                    {
+                        Count = 0,
+                        Page = request.Page,
+                        TotalPages = 0,
+                        List = new List<OrganizationDto>()
+                    };
+                }
 
                 // Fetch all OrgCodes from list  
-              
+
                 var orgCodes = data.List.Select(x => x.OrgCode).Distinct().ToList();
 
                 if (!orgCodes.Any())
